33abe2973cff59b5e0a603421a5c343444f7533b
AMOsPL <meliorat@poczta.fm>
2015-10-24 02:02:24 +0200

    Create controls_doWhileUntil.js

---
+++ test/spec/controls_doWhileUntil.js
@@ -0,0 +1,20 @@
+/* global describe, it, expect */
+(function() {
+	'use strict';
+
+	var root = this;
+
+	root.define([], function() {
+
+		describe('In Roboblocks.Blocks.controls_doWhileUntil block', function () {
+
+			it('generates expected default Arduino code', function () {
+				expect( true ).to.equal( true );
+				var bool = Blockly.Block.obtain(Blockly.mainWorkspace, 'controls_doWhileUntil');
+
+				assert.equal(Blockly.Arduino.workspaceToCode(), '/***   Global variables   ***/\n\n/***   Function declaration   ***/\n\nvoid setup()\n{\n\n}\n\n\nvoid loop()\n{\n  do {\n  } while ()\n}\n\n/***   Function definition   ***/\n' );
+			});
+		});
+	});
+
+}).call( this );
